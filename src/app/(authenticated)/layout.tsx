1a606eae3b50d9b4d35ab399b6771b30d6a4abaa
Zack Davis <zack.davis88@hotmail.com>
2024-10-11 14:07:56 -0500

    adding AuthenticatedLayout

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 import { AuthenticatedNavbar } from '@/app/components/Navbar';
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
   return (
     <Box display="flex" flexDirection="column" width="100%" height="100%">
       <AuthenticatedNavbar />
